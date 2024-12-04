028c3f84adac007be85d7b34cf0daef70e0984b5
Juan Rossano <juanrossano@live.com.ar>
2024-12-04 15:47:09 -0300

    Librerias para estilo de aplicacion

---
+++ src/app/forms/boards/boards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards',
+  imports: [],
+  templateUrl: './boards.component.html'
+})
+export class BoardsComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        exito: colors.green,
+        primario: colors.blue,
+        secundario: colors.indigo,
+        alerta: colors.red,
+      },
+      container: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1024px',
+          '2xl': '1536px',
+        },
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
-
